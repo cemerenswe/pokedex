19274391f14b8587a54f2f49f28007d4d7978f21
Cem <cem.oelm@gmail.com>
2026-06-01 18:21:15 +0200

    Add pokemon caching and load more functionality

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
-function onLoadFunc(){
-    loadPkmn()
+function onLoadFunc() {
+  loadPkmn();
 }
 
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
 
 let allPkmn = {};
 
+async function getPkmn(id) {
+  if (allPkmn[id]) {
+    return allPkmn[id];
+  }
+
+  let response = await fetch(BASE_URL + id);
+  let pokemonData = await response.json();
+
+  allPkmn[id] = pokemonData;
+  return pokemonData;
+}
+
+let currentStart = 40;
+let amount = 20;
+
 async function loadPkmn() {
-    let content = "";
-    for (let i = 40; i < 60; i++) {
-        let response = await fetch(BASE_URL + i);
-        let pokemonData = await response.json();
-        content += getPokemonTemplate(pokemonData);
-    }
-    document.getElementById("pokemonContainer").innerHTML += content;
-} 
+  let content = "";
+  for (let i = currentStart; i < currentStart + amount; i++) {
+    let pokemonData = await getPkmn(i);
+    content += getPokemonTemplate(pokemonData);
+  }
+  currentStart += amount;
+  document.getElementById("pokemonContainer").innerHTML += content;
+}
 
-
-
+document.getElementById("load-more-button").addEventListener("click", loadPkmn);
--- templates/templates.js
+++ templates/templates.js
@@ -1,7 +1,9 @@
-function getPokemonTemplate(pokemonData){
-    return `
+function getPokemonTemplate(pokemonData) {
+  return `
     <div class="pkmn-card">
-        <img src="${pokemonData.sprites.front_default}">
-        <h2>${pokemonData.name}</h2>
-    </div>`
+      <span>#${pokemonData.id}</span>
+      <img src="${pokemonData.sprites.front_default}">
+      <h2>${pokemonData.name}</h2>
+      <p>${pokemonData.types[0].type.name}</p>
+    </div>`;
 }
